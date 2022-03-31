e4d09b51de771369e56b1d06c973a625628c478f
Vidalatan <vidalatan@gmail.com>
2022-03-31 14:03:08 -0500

    Updated server.js
    Got delete and post persistence to work.

--- server.js
+++ server.js
@@ -12,23 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
 app.get('/api/notes', (req,res) => {
-    return res.json(db)
+    return res.json(dbfile.get("notes"))
 })
 
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
-
 function determineUsableID() {
     let idArr = []
-    for (item of dbfile.get()) {
+    for (item of dbfile.get("notes")) {
         idArr.push(item.id)
     }
     for (let index = 1; index < idArr.length+2;index++) {
@@ -40,9 +38,14 @@ function determineUsableID() {
 
 app.post('/api/notes', (req, res) => {
     console.log('Posted');
-    dbfile.append('', {...req.body, id: determineUsableID()})
-    dbfile.save()
-    return res.json(dbfile.get())
+    dbfile.append('notes', {...req.body, id: determineUsableID()})
+    return res.json(dbfile.get("notes"))
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    let temp = dbfile.get("notes").filter( note => note.id !== parseInt(req.params.id))
+    dbfile.set("notes", temp)
+    return res.json(dbfile.get("notes"))
 })
 
 app.listen(PORT, () => console.log('Listening'))
